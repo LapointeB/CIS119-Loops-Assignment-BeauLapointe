711380db50c88ccac4450649402621191e88214f
Owner <Owner@DESKTOP-E8ON95U>
2020-10-18 11:29:14 -0500

    It's the program

--- public_html/LoopsAssignment1.js
+++ public_html/LoopsAssignment1.js
@@ -3,5 +3,11 @@
  * 0, 10, 20, 30, ... 1000
  */
 
-document.write("This program displays the numbers 0 to 1000 by 10's");
+document.write("This program displays the numbers 0 to 1000 by 10's <br>");
 
+let int = 0;
+
+while (int <= 1000) {
+    document.write(int + "<br>");
+    int = int + 10;
+}
